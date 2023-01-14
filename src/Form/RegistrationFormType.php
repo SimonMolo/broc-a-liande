e8b7cb0eb52371d8f375ebf4763ebe727244d304
SimonMolo <lemartelotsimon@gmail.com>
2023-01-14 23:51:44 +0100

    css start accueil ok login ok register ok

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,7 +11,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use function Sodium\add;
 
 
 class RegistrationFormType extends AbstractType
@@ -22,14 +21,6 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('firstname')
             ->add('name')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('Password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
