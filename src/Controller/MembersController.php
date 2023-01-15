114200af8780164dc28820c077c254a4a206e14a
SimonMolo <lemartelotsimon@gmail.com>
2023-01-15 21:21:46 +0100

    Roles Ok

--- src/Controller/MembersController.php
+++ src/Controller/MembersController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
@@ -10,9 +11,17 @@ use App\Repository\UserRepository;
 class MembersController extends AbstractController
 {
     /**
-     * @Route ("/membershome", name="membersHome")
+     * @Route ("members/membershome", name="membersHome")
      */
     public function membersHome(){
         return $this ->render("members/memberHome.html.twig");
+    }
+
+    /**
+     * @Route("members/stand", name="memberStand")
+     */
+    public function stand(ArticlesRepository $articlesRepository){
+        $articles = $articlesRepository->findAll();
+        return $this ->render("members/stand.html.twig",['articles'=>$articles]);
     }
 }
