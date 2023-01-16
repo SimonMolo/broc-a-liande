41e1694e98a7e3e1abaf0f56d6f593a0eb5f03a3
SimonMolo <lemartelotsimon@gmail.com>
2023-01-16 12:56:49 +0100

    ArticlesType

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,27 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController{
 
+        /**
+     * @Route("admin/gestion", name="gestion")
+     */
+        public function gestion(){
+        return $this ->render('admin/adminStand.html.twig');
+    }
 
-/**
- * @Route("admin/gestion", name="gestion")
- */
-public function gestion(){
-    return $this ->render('admin/adminStand.html.twig');
-}
+        /**
+         * @Route('admin/addProduct", name="ajoutProduit")
+         */
+        public function addProduct(Request $request, EntityManagerInterface $entityManager){
+            $article = new article();
 
+        }
 }
